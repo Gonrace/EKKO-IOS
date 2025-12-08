bd547a84b274a9a8cba5b025e98c1308fb4bcc18
Paul Serra <ps.paulserra@gmail.com>
2025-12-08 15:08:47 +0100

    -> Clé ACR sécurisé -> Streaming pour creation du CSV -> Gestion des Erreurs

---
+++ EKKO/ErrorManager.swift
@@ -0,0 +1,8 @@
+//
+//  ErrorManager.swift
+//  EKKO
+//
+//  Created by Paul Serra on 08/12/2025.
+//
+
+import Foundation
---
+++ EKKO/StreamReader.swift
@@ -0,0 +1,7 @@
+//
+//  Untitled.swift
+//  EKKO
+//
+//  Created by Paul Serra on 08/12/2025.
+//
+
